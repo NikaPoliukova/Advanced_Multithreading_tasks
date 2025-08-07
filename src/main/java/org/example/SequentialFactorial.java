422bcfc6083bae302a229daf5b511c6c3dbfeff7
NikaPoliukova <nikanarchuk@gmail.com>
2025-08-07 17:07:13 +0400

    1 task

---
+++ src/main/java/org/example/SequentialFactorial.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import java.math.BigInteger;
+
+public class SequentialFactorial {
+    public BigInteger factorial(int n) {
+        BigInteger result = BigInteger.ONE;
+        for (int i = 2; i <= n; i++) {
+            result = result.multiply(BigInteger.valueOf(i));
+        }
+        return result;
+    }
+}
